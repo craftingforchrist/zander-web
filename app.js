8fb234e09d5cc9f2d544109b5f4dceaa5264176d
shadowolfyt <shadowolfbusiness@gmail.com>
2019-05-01 12:06:01 +1000

    Fixed issue with css not loading on profile pages.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var session;
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 const connection = mysql.createConnection({
   host: process.env.databasehost || config.databasehost,
