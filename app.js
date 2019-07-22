fd7111030380370327412ea29dc0ecfca0d7306b
benrobson8 <csceg12188>
2019-07-22 23:14:11 +1000

    Updated packages and made site colour customiseable.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mysql = require('mysql');
 const ejs = require('ejs');
 const package = require('./package.json');
 const config = require('./config.json');
-// const credentials = require('./credentials.json');
+const credentials = require('./credentials.json');
 const request = require('request');
 const Discord = require('discord.js');
 const client = new Discord.Client({
@@ -31,7 +31,8 @@ const connection = mysql.createConnection({
   host: process.env.dbhost || credentials.dbhost,
   user: process.env.dbuser || credentials.dbuser,
   password: process.env.dbpassword || credentials.dbpassword,
-  database: process.env.dbdatabase || credentials.dbdatabase
+  database: process.env.dbdatabase || credentials.dbdatabase,
+  multipleStatements: true
 });
 
 connection.connect(function(err) {
@@ -48,6 +49,7 @@ connection.connect(function(err) {
 app.get('/', function (req, res) {
   res.render('index', {
     "servername": `${config.servername}`,
+    "sitecolour": `${config.sitecolour}`,
     "email": `${config.email}`,
     "pagetitle": "Home"
   });
@@ -66,6 +68,7 @@ app.get('/donate', function (req, res) {
 app.get('/apply', function (req, res) {
   res.render('apply', {
     "servername": `${config.servername}`,
+    "sitecolour": `${config.sitecolour}`,
     "email": `${config.email}`,
     "pagetitle": "Apply"
   });
@@ -98,6 +101,7 @@ app.post('/apply', urlencodedParser, function (req, res) {
 app.get('/report', function (req, res) {
   res.render('report', {
     "servername": `${config.servername}`,
+    "sitecolour": `${config.sitecolour}`,
     "email": `${config.email}`,
     "pagetitle": "Report a Player"
   });
@@ -144,6 +148,7 @@ app.get('/issues', function (req, res) {
 app.get('/rules', function (req, res) {
   res.render('rules', {
     "servername": `${config.servername}`,
+    "sitecolour": `${config.sitecolour}`,
     "email": `${config.email}`,
     "pagetitle": "Rules",
     rulesmd: config.rulesmd
@@ -162,8 +167,10 @@ app.get('/development/plugin', function (req, res) {
   function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
       var info = JSON.parse(body);
+      res.redirect('/');
       res.render('development-plugin', {
         "servername": `${config.servername}`,
+        "sitecolour": `${config.sitecolour}`,
         "email": `${config.email}`,
         "pagetitle": "Plugin Development Log",
         objdata: info
@@ -187,6 +194,7 @@ app.get('/development/web', function (req, res) {
       var info = JSON.parse(body);
       res.render('development-web', {
         "servername": `${config.servername}`,
+        "sitecolour": `${config.sitecolour}`,
         "email": `${config.email}`,
         "pagetitle": "Web Development Log",
         objdata: info
@@ -200,19 +208,21 @@ app.get('/development/web', function (req, res) {
 // Players
 //
 app.get('/players', function (req, res) {
-  let sql = `SELECT * FROM playerdata`;
-  connection.query (sql, function (err, result) {
-    if (err) {
+  connection.query (`SELECT * FROM playerdata; select pd.username as 'username', count(ses.id) as 'joins' from sessions ses left join playerdata pd on pd.id = ses.player_id group by pd.username;`, function (error, results, fields) {
+    if (error) {
       res.redirect('/');
-      throw err;
+      throw error;
     } else {
       res.render('players', {
         "servername": `${config.servername}`,
+        "sitecolour": `${config.sitecolour}`,
         "email": `${config.email}`,
         "pagetitle": "Players",
-        objdata: result
+        objdata: results
       });
     }
+    console.log(results[0]);
+    console.log(results[1]);
   });
 });
 
@@ -228,6 +238,7 @@ app.get('/punishments', function (req, res) {
     } else {
       res.render('punishments', {
         "servername": `${config.servername}`,
+        "sitecolour": `${config.sitecolour}`,
         "email": `${config.email}`,
         "pagetitle": "Punishments",
         objdata: result
@@ -248,6 +259,7 @@ app.get('/profile/:username', function (req, res) {
     } else {
       res.render('profile', {
         "servername": `${config.servername}`,
+        "sitecolour": `${config.sitecolour}`,
         "email": `${config.email}`,
         "pagetitle": `${req.params.username}'s Profile`,
         objdata: result
