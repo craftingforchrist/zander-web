9f049dcc019bc466c618188a777d45a56c181009
benrobson8 <csceg12188>
2019-08-17 20:29:20 +1000

    Added a website discord command.

---
+++ discord/commands/website.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const config = require('../../config.json')
+
+module.exports.run = async (client, message, args) => {
+  var embed = new Discord.RichEmbed()
+    .setTitle(`Website`)
+    .setURL(`${config.website}`)
+    .setDescription(`You can our website here: ${config.website}`)
+    .setColor('#ffa366')
+  message.channel.send(embed);
+};
+
+module.exports.help = {
+  name: 'website'
+};
