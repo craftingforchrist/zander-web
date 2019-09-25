3127b57ed9a0da09075ddb30e53b305e934f9090
benrobson8 <benrobson76@gmail.com>
2019-09-26 08:34:50 +1000

    Started working on backend function.

--- controllers/rcon.js
+++ controllers/rcon.js
@@ -2,8 +2,7 @@ const Rcon = require('modern-rcon');
 const rcon = new Rcon(`${process.env.rconaddress}`, `${process.env.rconpassword}`);
 const chalk = require('chalk');
 
-rcon.connect().then(() => {
-  console.log('Remote Connection has been made.');
-});
+rcon.connect();
+console.log('Remote Connection has been made.');
 
 module.exports = rcon;
