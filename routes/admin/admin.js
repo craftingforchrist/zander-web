102b26231c7a4ae65f65f0afc6c0b1a97c709cca
shadowolfyt <shadowolfbusiness@gmail.com>
2019-09-25 23:05:57 +1000

    Starting form implementation.

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -5,8 +5,16 @@ const database = require('../../controllers/database.js');
 
 router.get('/', function(req, res, next) {
   res.render('admin/admin', {
-    "pagetitle": "Admin"
+    "pagetitle": "Administration Panel"
   });
+});
+
+router.post('/', function (req, res) {
+  const action = req.body.action;
+  const username = req.body.username;
+  const method = req.body.method;
+
+  console.log(req.body);
 });
 
 module.exports = router;
