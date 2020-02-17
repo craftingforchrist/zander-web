99f9781df6b6f8ffcbf2d565083b580f411e420d
benrobson8 <benrobson76@gmail.com>
2020-02-18 00:59:01 +1100

    Adding things you won't see for a long time.

---
+++ routes/play.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const config = require('../config.json');
+const database = require('../controllers/database.js');
+
+router.get('/', (req, res, next) => {
+  database.query (`SELECT * FROM servers ORDER BY id DESC;`, function (error, results, fields) {
+    if (error) {
+      res.redirect('/');
+      throw error;
+    } else {
+      res.render('play', {
+        "pagetitle": "Play",
+        objdata: results
+      });
+    };
+  });
+});
+
+module.exports = router;
