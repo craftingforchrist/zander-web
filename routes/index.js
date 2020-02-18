139e00670dc330f2abd3ec955504fdd7e5ecfe82
benrobson8 <benrobson76@gmail.com>
2020-02-18 11:01:54 +1100

    Last changes.

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const config = require('../config.json');
+const database = require('../controllers/database.js');
+
+router.get('/', (req, res, next) => {
+  database.query (`SELECT * FROM ccstreams WHERE status = "ONLINE";`, function (error, results, fields) {
+    if (error) {
+      res.redirect('/');
+      throw error;
+    } else {
+      res.render('index', {
+        "pagetitle": "Home",
+        objdata: results
+      });
+    };
+  });
+});
+
+module.exports = router;
