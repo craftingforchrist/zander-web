53252157a6a9944b1cf149b6f3b3f4429a201e4b
benrobson <efg/998>
2020-04-25 12:49:54 +1000

    Redirect to new guides site.

--- routes/guides.js
+++ routes/guides.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const config = require('../config.json');
 
 router.get('/', (req, res, next) => {
-  res.render('guides', {
-    "pagetitle": "Guides"
-  });
+  // res.render('guides', {
+  //   "pagetitle": "Guides"
+  // });
+  res.redirect('https://guides.craftingforchrist.net/');
 });
 
 module.exports = router;
