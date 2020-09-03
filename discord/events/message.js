1f76edeb68187eb898a440ced227909f045244b4
benrobson <efg/998>
2020-09-03 22:23:17 +1000

    General Kenobi

---
+++ discord/events/message.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const config = require('../../config.json')
+
+module.exports = async message => {
+  if (message.content.includes("hello there")) {
+    let embed = new Discord.MessageEmbed()
+    .attachFiles("https://media.giphy.com/media/8JTFsZmnTR1Rs1JFVP/giphy.gif")
+    message.channel.send(embed);
+    return;
+  }
+}
