7962f39ba3f53722ca3572e7a73865c0b973c9fc
Aidan <69904509+SuperSilly12356@users.noreply.github.com>
2020-09-03 22:36:17 +1000

    General Kenobi capital hello

--- discord/events/message.js
+++ discord/events/message.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const config = require('../../config.json')
 
 module.exports = async message => {
-  if (message.content.includes("hello there")) {
+  if (message.content.includes("hello there") || message.content.includes("Hello there")) {
     let embed = new Discord.MessageEmbed()
     .attachFiles("https://media.giphy.com/media/8JTFsZmnTR1Rs1JFVP/giphy.gif")
     message.channel.send(embed);
