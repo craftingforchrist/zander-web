7aab2f0b9f2e248abf0dca6fbd2bc7e544b8be73
benrobson <benrobson76@gmail.com>
2020-09-24 21:07:59 +1000

    Added an upcoming event section to the home page.

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const moment = require('moment');
 const router = express.Router();
 const config = require('../config.json');
 const database = require('../controllers/database.js');
 
 router.get('/', (req, res, next) => {
-  database.query (`SELECT COUNT(*) as ccstreamslive FROM ccstreams WHERE status = "ONLINE";`, function (error, results, fields) {
+  database.query (`SELECT COUNT(*) as ccstreamslive FROM ccstreams WHERE status = "ONLINE"; SELECT * FROM events ORDER BY eventdatetime DESC LIMIT 1;`, function (error, results, fields) {
     if (error) {
       res.redirect('/');
       throw error;
     } else {
       res.render('index', {
         "pagetitle": "Home",
-        objdata: results
+        objdata: results,
+        event: results[1][0],
+        moment: moment
       });
     };
   });
