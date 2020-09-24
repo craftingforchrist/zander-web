8e390f1969d4a6dfc088ecb4b2c0b6ad75be1904
benrobson <benrobson76@gmail.com>
2020-09-24 21:14:46 +1000

    Disable edit event broadcast.

--- routes/admin/events/edit.js
+++ routes/admin/events/edit.js
@@ -34,17 +34,17 @@ module.exports = (client) => {
         } else {
           res.redirect('/admin/events');
 
-          // Take event that was edited and announce updates.
-          let eventschannel = client.channels.cache.find(c => c.name === `${config.eventschannel}`);
-          if (!eventschannel) return console.log(`A #${config.eventschannel} channel does not exist.`);
-
-          var embed = new Discord.MessageEmbed()
-            .setTitle(`Event Update`)
-            .setDescription(`The ${title} event information has been updated.\nGo to ${config.website}events to see more information.`)
-            .setThumbnail(icon)
-            .setColor(HexColour.eventedited)
-          eventschannel.send(embed);
-          console.log(`[CONSOLE] [DISCORD] The new Event has been broadcasted to Discord.`);
+          // // Take event that was edited and announce updates.
+          // let eventschannel = client.channels.cache.find(c => c.name === `${config.eventschannel}`);
+          // if (!eventschannel) return console.log(`A #${config.eventschannel} channel does not exist.`);
+          //
+          // var embed = new Discord.MessageEmbed()
+          //   .setTitle(`Event Update`)
+          //   .setDescription(`The ${title} event information has been updated.\nGo to ${config.website}events to see more information.`)
+          //   .setThumbnail(icon)
+          //   .setColor(HexColour.eventedited)
+          // eventschannel.send(embed);
+          // console.log(`[CONSOLE] [DISCORD] The new Event has been broadcasted to Discord.`);
         };
       });
     } else {
