e62ffd7622f18bed2e211d1352679ec37faea229
benrobson <benrobson76@gmail.com>
2020-09-24 20:16:11 +1000

    Added features to Event Manager. - Broadcasting to Discord when Event created, edited or removed. - Purge old Events daily at 7AM

--- cron/clearOldEvents.js
+++ cron/clearOldEvents.js
@@ -1,14 +1,15 @@
 const cron = require('node-cron');
+const moment = require('moment');
 const database = require('../controllers/database');
 
 // Cron Job
 // This will fire once a day to clear all events that are past.
-const task = cron.schedule("0 0 0 * * *", function() {
-  database.query (`truncate votes`, function (err, results) {
+const task = cron.schedule('0 7 * * *', function() {
+  database.query (`DELETE FROM events WHERE eventdatetime < "${moment().format('YYYY-MM-DD HH:mm:ss')}"`, function (err, results) {
     if (err) {
       throw err;
     } else {
-      console.log(`[CONSOLE] [CRON] Old events have been cleared.`);
+      console.log(`[CONSOLE] [CRON] Old Events have been cleared.`);
     }
   });
 }, {
