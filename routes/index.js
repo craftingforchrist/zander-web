fcc9879a6d943c08a80c657a24a602a0bce8100c
benrobson <benrobson76@gmail.com>
2020-10-21 12:39:40 +1100

    Hide disabled servers.

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const database = require('../controllers/database.js');
 router.get('/', (req, res, next) => {
   let sql = `SELECT COUNT(*) as ccstreamslive FROM ccstreams WHERE status = "ONLINE";
   SELECT * FROM events ORDER BY eventdatetime DESC LIMIT 1;
-  SELECT * FROM servers ORDER BY position ASC;`
+  SELECT * FROM servers WHERE visable = true ORDER BY position ASC;`
 
   database.query (sql, function (error, results, fields) {
     if (error) {
