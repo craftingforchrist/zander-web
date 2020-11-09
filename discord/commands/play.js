4f52326f0824de258345f401cf5a12a958ba2ab0
benrobson <benrobson76@gmail.com>
2020-11-09 17:46:50 +1100

    Add a !play command to Discord, list all avaliable Servers.

---
+++ discord/commands/play.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+const HexColour = require('../../HexColour.json');
+const database = require('../../controllers/database.js'); // Database Controller
+
+module.exports.run = async (client, message, args) => {
+    //
+    // Database Entry
+    //
+    let sql = `SELECT * FROM servers WHERE visable = true ORDER BY position ASC;`;
+    database.query (sql, function (err, results) {
+      if (err) {
+        throw err;
+      } else {
+        console.log(results);
+
+        let embed = new Discord.MessageEmbed()
+        .setTitle(`${args[0]}'s Connected Accounts`)
+        .setColor(HexColour.yellow)
+
+        results.forEach(function(serverdata) {
+          embed.addField(`${serverdata.name} [${serverdata.ipaddress}]`, `${serverdata.description}`)
+        })
+        message.channel.send(embed);
+      }
+    });
+};
+
+module.exports.help = {
+  name: 'play',
+  description: 'Lists all Servers that are in the Server list.',
+  usage: 'play'
+};
